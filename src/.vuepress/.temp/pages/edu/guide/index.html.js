35f236ffd7bb57ab332107c0f53db3e80507b53b
CodingLifeV <1013801973@qq.com>
2023-02-02 19:45:06 +0800

    change homepage

--- src/.vuepress/.temp/pages/edu/guide/index.html.js
+++ src/.vuepress/.temp/pages/edu/guide/index.html.js
@@ -1,1 +1,14 @@
-export const data = JSON.parse("{\"key\":\"v-025ed4e5\",\"path\":\"/edu/guide/\",\"title\":\"学习指南\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"学习指南\",\"copyright\":false,\"footer\":\"MIT Licensed  <a href=\\\"https://beian.miit.gov.cn/#/Integrated/index\\\" target=\\\"_blank\\\">浙ICP备2022028662号-1</a> | Copyright © 2022-present <a href=\\\"https://github.com/CodingLifeVV\\\" target=\\\"_blank\\\">CodingLifeVV</a> </p> 内容创作不易，引用请注明出处，网站已备案，切勿侵权\",\"summary\":\"项目基础入门 项目架构及功能概述; 学习路线;\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://www.bmrs.top/edu/guide/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"编码人生\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"学习指南\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:updated_time\",\"content\":\"2022-11-03T03:01:23.000Z\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"en-US\"}],[\"meta\",{\"property\":\"article:modified_time\",\"content\":\"2022-11-03T03:01:23.000Z\"}]]},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"项目基础入门\",\"slug\":\"项目基础入门\",\"link\":\"#项目基础入门\",\"children\":[]}],\"git\":{\"createdTime\":1667444483000,\"updatedTime\":1667444483000,\"contributors\":[{\"name\":\"CodingLifeVV\",\"email\":\"wyj_program@163.com\",\"commits\":1}]},\"readingTime\":{\"minutes\":0.28,\"words\":83},\"filePathRelative\":\"edu/guide/README.md\",\"localizedDate\":\"November 3, 2022\"}")
+export const data = JSON.parse("{\"key\":\"v-025ed4e5\",\"path\":\"/edu/guide/\",\"title\":\"学习指南\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"学习指南\",\"copyright\":false,\"footer\":\"MIT Licensed  <a href=\\\"https://beian.miit.gov.cn/#/Integrated/index\\\" target=\\\"_blank\\\">浙ICP备2022028662号-1</a> | Copyright © 2022-present <a href=\\\"https://github.com/CodingLifeVV\\\" target=\\\"_blank\\\">CodingLifeVV</a> </p> 内容创作不易，引用请注明出处，网站已备案，切勿侵权\",\"summary\":\"项目基础入门 项目架构及功能概述; 学习路线;\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://www.bmrs.top/edu/guide/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"编码人生\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"学习指南\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"en-US\"}]]},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"项目基础入门\",\"slug\":\"项目基础入门\",\"link\":\"#项目基础入门\",\"children\":[]}],\"readingTime\":{\"minutes\":0.28,\"words\":83},\"filePathRelative\":\"edu/guide/README.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
--- src/.vuepress/.temp/pages/interview/index.html.js
+++ src/.vuepress/.temp/pages/interview/index.html.js
@@ -1,1 +1,14 @@
-export const data = JSON.parse("{\"key\":\"v-71b3ae87\",\"path\":\"/interview/\",\"title\":\"Java面试知识总结\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Java面试知识总结\",\"copyright\":false,\"footer\":\"MIT Licensed  <a href=\\\"https://beian.miit.gov.cn/#/Integrated/index\\\" target=\\\"_blank\\\">浙ICP备2022028662号-1</a> | Copyright © 2022-present <a href=\\\"https://github.com/CodingLifeVV\\\" target=\\\"_blank\\\">CodingLifeVV</a> </p> 内容创作不易，引用请注明出处，网站已备案，切勿侵权\",\"summary\":\"目录 Java基础; Java框架-SSM; 数据库; 计算机网络; Redis; 多线程; 设计模式; Java虚拟机; 剑指Offer算法;\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://www.bmrs.top/interview/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"编码人生\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Java面试知识总结\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:updated_time\",\"content\":\"2023-02-01T01:12:44.000Z\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"en-US\"}],[\"meta\",{\"property\":\"article:modified_time\",\"content\":\"2023-02-01T01:12:44.000Z\"}]]},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"目录\",\"slug\":\"目录\",\"link\":\"#目录\",\"children\":[]}],\"git\":{\"createdTime\":1674210095000,\"updatedTime\":1675213964000,\"contributors\":[{\"name\":\"CodingLifeV\",\"email\":\"1013801973@qq.com\",\"commits\":2}]},\"readingTime\":{\"minutes\":0.36,\"words\":108},\"filePathRelative\":\"interview/README.md\",\"localizedDate\":\"January 20, 2023\"}")
+export const data = JSON.parse("{\"key\":\"v-71b3ae87\",\"path\":\"/interview/\",\"title\":\"Java面试知识总结\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Java面试知识总结\",\"copyright\":false,\"footer\":\"MIT Licensed  <a href=\\\"https://beian.miit.gov.cn/#/Integrated/index\\\" target=\\\"_blank\\\">浙ICP备2022028662号-1</a> | Copyright © 2022-present <a href=\\\"https://github.com/CodingLifeVV\\\" target=\\\"_blank\\\">CodingLifeVV</a> </p> 内容创作不易，引用请注明出处，网站已备案，切勿侵权\",\"summary\":\"目录 Java基础; Java框架-SSM; 数据库; 计算机网络; Redis; 多线程; 设计模式; Java虚拟机; 剑指Offer算法;\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://www.bmrs.top/interview/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"编码人生\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Java面试知识总结\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"en-US\"}]]},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"目录\",\"slug\":\"目录\",\"link\":\"#目录\",\"children\":[]}],\"readingTime\":{\"minutes\":0.36,\"words\":108},\"filePathRelative\":\"interview/README.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
